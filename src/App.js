9714bbca99320f1fc9e6e779b3ea9a68abe39924
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2020-02-11 19:24:46 -0800

    Tested Bitdev on local machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PrimaryButton, { LoadingTextButton, SuccessButton, DangerButton, RoundSpinnerButton } from '@bit/lekanmedia.shared-ui.primary-button';
+
+const style = {
+	margin: '20px',
+	display: 'flex',
+	justifyContent: 'center',
+  alignItem: 'center',
+  width: '200px'
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+		<div style={style}>
+			<PrimaryButton text="Primary Button" />
+		</div>
+		<div style={style}>
+			<LoadingTextButton spinnerSize={12} />
+		</div>
+		<div style={style}>
+			<RoundSpinnerButton spinnerColors={['#3b73ff', '#5cb85c', '#d9534f', '#910ac7']} spinnerSize={25} />
+		</div>
+		<div style={style}>
+			<SuccessButton />
+		</div>
+		<div style={style}>
+			<DangerButton />
+		</div>
+	</section>
   );
 }
 
